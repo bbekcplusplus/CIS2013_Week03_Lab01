deffff3167dea7efd0bcc296fb87129815619432
Bibek Gupta <bibek.gupta@okstate.edu>
2018-01-31 20:13:06 -0600

    CIS2013_Week03_Lab01
    do while loop

--- CIS2013_Week03_Lab01/FlowControl.cpp
+++ CIS2013_Week03_Lab01/FlowControl.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main() {
+	char continueProgram = 'a';
 
+	do {
+		cout << "Do you want to continue (y/n): ";
+		cin >> continueProgram;
+
+		switch (continueProgram == 'y') {
+		case 'y':
+			cout << "Going to continue";
+			break;
+		case 'n':
+			cout << "Going to end";
+			break;
+		default:
+			continueProgram = 'y';
+		}
+		}while (continueProgram == 'y');
+		return 0;
+	
 
 }
